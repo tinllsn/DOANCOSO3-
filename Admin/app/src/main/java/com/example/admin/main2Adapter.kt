f35c62e31dc9152a292fdc8275cb53619fbb814f
thuylanh04 <vothithuylanh2004@gmail.com>
2024-06-14 09:17:06 +0700

    update interface

--- Admin/app/src/main/java/com/example/admin/main2Adapter.kt
+++ Admin/app/src/main/java/com/example/admin/main2Adapter.kt
@@ -10,7 +10,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+//import com.bumptech.glide.Glide
 import com.example.admin.databinding.ActivityMain2Binding
 
 
